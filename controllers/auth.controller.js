4f5cb67737ac259f9f42620f86a130a98bb2b342
Jonny0430 <ahadjon697gmail.com>
2025-06-08 22:39:31 +0900

    feat: database and middleware & SMTP

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,26 @@
+
+const mailService = require('../service/mail.service')
+const userModel = require('../models/user.model')
+
+
 class AuthController {
 	async login(req, res, next) {
 		const { email } = req.body
 		res.json({ email })
+		try {
+			const { email } = req.body
+			await mailService.sendOtp(email)
+			// const existUser = await userModel.findOne({ email })
+			// if (existUser) {
+			// 	throw BaseError.BadRequest('User already exist', [{ email: 'User already exist' }])
+			// }
+			// const createdUser = await userModel.create({ email })
+			res.status(201).json({ email })
+		} catch (error) {
+			next(error)
+		}
 	}
+
 	async verify(req, res, next) {
 		const { email, otp } = req.body
 		res.json({ email, otp })
